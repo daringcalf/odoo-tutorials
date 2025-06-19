f1bee443e030aa940ae4dfee0ba29e44632214f9
Daring Cλlf <daringcalf@outlook.com>
2025-06-19 21:15:59 +0800

    [ADD] estate module: create initial files for estate management including models and manifest

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Estate Management",
+    "application": True,
+    "depends": [
+        "base",
+    ],
+    "category": "Tutorials/Estate",
+    "license": "AGPL-3",
+}
