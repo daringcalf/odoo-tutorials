4add9f9ef00e8a34208f88e20e5bb30346f8ee0d
Daring Cλlf <daringcalf@outlook.com>
2025-06-20 21:57:19 +0800

    [ADD] estate module: root menu and action views with field defaults and constraints

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,15 +4,17 @@ from odoo import models, fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "Estate Property"
+    _description = "Properties"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -26,4 +28,19 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
         help="The orientation of the garden",
+    )
+    active = fields.Boolean(default=True)
+    # New, Offer Received, Offer Accepted, Sold and Cancelled.
+    state = fields.Selection(
+        string="Status",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        copy=False,
+        required=True,
     )
