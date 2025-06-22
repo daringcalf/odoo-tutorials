f144c891e3c6a0cbe247949d6112b69c4c4f240c
Daring Cλlf <daringcalf@outlook.com>
2025-06-22 21:07:27 +0800

    [ADD] estate module: implement actions to accept and refuse property offers

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -155,3 +155,16 @@ class EstatePropertyOffer(models.Model):
                 ).days
             else:
                 offer.validity = 7
+
+    # Actions
+    def action_accept_offer(self):
+        for offer in self:
+            offer.status = "accepted"
+            offer.property_id.state = "offer_accepted"
+            offer.property_id.buyer_id = offer.partner_id
+            offer.property_id.selling_price = offer.price
+
+    def action_refuse_offer(self):
+        for offer in self:
+            offer.status = "refused"
+            offer.status = "refused"
